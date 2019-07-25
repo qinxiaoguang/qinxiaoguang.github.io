3da7251a230df258bb18baaa57e5858c30ddcf83
qinxiaoguang01 <qinxiaoguang01@baidu.com>
2019-07-25 09:51:21 +0800

    update

--- static/js/orgscript.js
+++ static/js/orgscript.js
@@ -1,4 +1,14 @@
 $(function(){
+    // table of contents 换个名字
+    tocNames = [
+        '🌹','🍀','🍎','🌙','🍁','🍂','🌷','👍','🔥','😀','😆','😘','😝','😸','🌑','🌒','🌓','🌔','🌕','🌖','🌗','🌘','🌚','🌛','🌜','🌝','🍉'
+    ]
+    rand = Math.floor((Math.random()*tocNames.length));
+    tocName = tocNames[rand];
+    document.getElementById('table-of-contents').getElementsByTagName('h2')[0].innerHTML= tocName + '\n<a class="close-sidebar" href="#">Close</a>';
+    document.getElementById('toggle-sidebar').getElementsByTagName('h2')[0].innerHTML= tocName;
+    
+    // 适应高
     var screenHeight = document.documentElement.clientHeight
     var contentHeight = $("#content").height();
     if (contentHeight < screenHeight) {
